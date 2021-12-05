76f3d92ab4bb11cf661ba36d5a01989ca0846eee
MikhailUskin <wurty@mail.ru>
2021-12-06 00:44:01 +0300

    add: mouse button event handling

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 use glam::Vec2;
-use ggez::{conf, event::{self, EventHandler}, Context, GameResult,
-    graphics::{self, DrawParam, Image}};
+use ggez::{conf, Context, GameResult,
+        event::{self, EventHandler, MouseButton}, 
+        graphics::{self, DrawParam, Image}};
 use specs::{
-    join::Join, Builder, Component, ReadStorage, RunNow, System, VecStorage, World, WorldExt};
+        join::Join, Builder, Component, ReadStorage, RunNow, System, VecStorage, World, WorldExt, Write, WriteStorage,};
 use std::path;
 
 mod rules;
+use rules::*;
 
 const TILE_WIDTH: f32 = 10.0;
 const UNIVERSE_WIDTH: usize = 50;
 const UNIVERSE_HEIGHT: usize = 50;
 
+const ALIVE_CELL_TILE_PATH: &str = "/images/alive_cell.png";
+const DEAD_CELL_TILE_PATH: &str = "/images/dead_cell.png";
+
 // This struct will hold all our game state
-struct Game {
+struct GameState {
     world: World,
 }
 
@@ -20,9 +25,15 @@ struct Game {
 // two things:
 // - updating
 // - rendering
-impl EventHandler for Game {
+impl EventHandler for GameState {
     fn update(&mut self, _context : &mut Context) -> GameResult<()> {
-        // TODO: update game logic here
+
+        // Run input system
+        {
+            let mut is = InputSystem {};
+            is.run_now(&self.world);
+        }
+
         Ok(())
     }
 
@@ -35,14 +46,31 @@ impl EventHandler for Game {
 
         Ok(())
     }
+
+    fn mouse_button_down_event(&mut self, _ctx: &mut Context, button: MouseButton, x: f32, y: f32) {
+        println!("Mouse button pressed: {:?}, x: {}, y: {}", button, x, y);
+
+        let mut input_queue = self.world.write_resource::<InputQueue>();
+
+        // Filter mouse presses
+        match button {
+            MouseButton::Left => input_queue.mouse_button_events.push(MouseButton::Left),
+            MouseButton::Right => input_queue.mouse_button_events.push(MouseButton::Right),
+            _ => return,
+        }
+
+        input_queue.pressed_cell_positions.push(Position {
+            x: (x / TILE_WIDTH) as u8,
+            y: (y / TILE_WIDTH) as u8,
+        });
+    }
 }
 
-#[derive(Debug, Component, Clone, Copy)]
+#[derive(Debug, Component)]
 #[storage(VecStorage)]
 pub struct Position {
     x: u8,
     y: u8,
-    z: u8,
 }
 
 #[derive(Component)]
@@ -51,41 +79,29 @@ pub struct Renderable {
     path: String,
 }
 
-#[derive(Component)]
-#[storage(VecStorage)]
-pub struct DeadCell {}
-
-#[derive(Component)]
-#[storage(VecStorage)]
-pub struct AliveCell {}
-
 pub fn register_components(world: &mut World) {
     world.register::<Position>();
     world.register::<Renderable>();
-    world.register::<DeadCell>();
-    world.register::<AliveCell>();
 }
 
-pub fn create_dead_cell(world: &mut World, position: Position) {
+pub fn create_cell<'a>(world: &mut World, position: Position, path: String) {
     world
         .create_entity()
-        .with(Position { z: 10, ..position })
-        .with(Renderable {
-            path: "/images/dead_cell.png".to_string(),
+        .with(Position { 
+            ..position 
         })
-        .with(DeadCell {})
+        .with(Renderable {
+            path: path,
+        })
         .build();
 }
 
-pub fn create_alive_cell(world: &mut World, position: Position) {
-    world
-        .create_entity()
-        .with(Position { z: 10, ..position })
-        .with(Renderable {
-            path: "/images/alive_cell.png".to_string(),
-        })
-        .with(AliveCell {})
-        .build();
+pub fn create_alive_cell(world: &mut World, position: Position){
+    create_cell(world, position, String::from(ALIVE_CELL_TILE_PATH));
+}
+
+pub fn create_dead_cell(world: &mut World, position: Position){
+    create_cell(world, position, String::from(DEAD_CELL_TILE_PATH));
 }
 
 pub struct RenderingSystem<'a> {
@@ -94,8 +110,10 @@ pub struct RenderingSystem<'a> {
 
 // System implementation
 impl<'a> System<'a> for RenderingSystem<'a> {
-    // Data
-    type SystemData = (ReadStorage<'a, Position>, ReadStorage<'a, Renderable>);
+    type SystemData = (
+        ReadStorage<'a, Position>, 
+        ReadStorage<'a, Renderable>,
+    );
 
     fn run(&mut self, data: Self::SystemData) {
         let (positions, renderables) = data;
@@ -103,10 +121,8 @@ impl<'a> System<'a> for RenderingSystem<'a> {
         // Clearing the screen (this gives us the background colour)
         graphics::clear(self.context, graphics::Color::new(0.95, 0.95, 0.95, 1.0));
 
-        // Get all the renderables with their positions and sort by the position z
-        // This will allow us to have entities layered visually.
+        // Get all the renderables with their positions
         let mut rendering_data = (&positions, &renderables).join().collect::<Vec<_>>();
-        rendering_data.sort_by_key(|&k| k.0.z);
 
         // Iterate through all pairs of positions & renderables, load the image
         // and draw it at the specified position.
@@ -131,17 +147,16 @@ impl<'a> System<'a> for RenderingSystem<'a> {
 pub fn initialize_level(world: &mut World) {
     let initial_generation = rules::Universe::new(UNIVERSE_WIDTH, UNIVERSE_HEIGHT).seed_initial_generation();
 
-    for (y, row) in initial_generation.iter().enumerate(){
-        for (x, cell) in row.iter().enumerate(){
+    for (cell_y, row) in initial_generation.iter().enumerate(){
+        for (cell_x, is_alive) in row.iter().enumerate(){
             // Create the position at which to create something on the map
             let position = Position {
-                x: x as u8,
-                y: y as u8,
-                z: 0,
+                x: cell_x as u8,
+                y: cell_y as u8,
             };
 
             // Figure out what object we should create
-            if *cell {
+            if *is_alive {
                 create_alive_cell(world, position);
             }
             else {
@@ -149,12 +164,96 @@ pub fn initialize_level(world: &mut World) {
             }
         }
     }
+
+    world.insert(initial_generation);
+}
+
+// Resources
+#[derive(Default)]
+pub struct InputQueue {
+    pub mouse_button_events: Vec<MouseButton>,
+    pub pressed_cell_positions: Vec<Position>,
+}
+
+// Registering resources
+pub fn register_resources(world: &mut World) {
+    world.insert(InputQueue::default());
+}
+
+pub struct InputSystem {}
+
+impl<'a> System<'a> for InputSystem {
+    type SystemData = (
+        Write<'a, InputQueue>,
+        Write<'a, Generation>,
+        WriteStorage<'a, Renderable>,
+        ReadStorage<'a, Position>,
+    );
+
+    fn run(&mut self, data: Self::SystemData) {
+        let (mut input_queue, mut generation, mut renderables, positions) = data;
+
+        while 
+            input_queue.mouse_button_events.len() > 0 && 
+            input_queue.pressed_cell_positions.len() > 0 {
+
+            let button_optional = input_queue.mouse_button_events.pop();
+            if !button_optional.is_some()
+            {
+                continue;
+            }
+
+            let pressed_position_optional = input_queue.pressed_cell_positions.pop();
+            if !pressed_position_optional.is_some()
+            {
+                continue;
+            }
+
+            let button = button_optional.unwrap();
+            let pressed_position = pressed_position_optional.unwrap();
+
+            let pressed_cell_x = pressed_position.x;
+            let pressed_cell_y = pressed_position.y;
+
+            match button {
+                MouseButton::Left => generation[pressed_cell_y as usize][pressed_cell_x as usize] = CELL_IS_ALIVE,
+                MouseButton::Right => generation[pressed_cell_y as usize][pressed_cell_x as usize] = CELL_IS_DEAD,
+                _ => ()
+            }
+
+            for (rendrable, renderable_cell_position) in (&mut renderables, &positions).join() {
+                if renderable_cell_position.x != pressed_cell_x {
+                    continue;
+                }
+
+                if renderable_cell_position.y != pressed_cell_y {
+                    continue;
+                }
+
+                match button {
+                    MouseButton::Left => rendrable.path = String::from(ALIVE_CELL_TILE_PATH),
+                    MouseButton::Right => rendrable.path = String::from(DEAD_CELL_TILE_PATH),
+                    _ => ()
+                }
+            }
+        }
+    }
+}
+
+fn generate_game_state() -> GameState {
+    let mut world = World::new();
+    register_components(&mut world);
+    register_resources(&mut world);
+    initialize_level(&mut world);
+
+    return GameState {
+        world
+    };
 }
 
 pub fn main() -> GameResult {
-    let mut world = World::new();
-    register_components(&mut world);
-    initialize_level(&mut world);
+    // Create the game state
+    let game = generate_game_state();
 
     // Create a game context and event loop
     let context_builder = ggez::ContextBuilder::new("rust_game_of_life", "game_of_life")
@@ -163,9 +262,6 @@ pub fn main() -> GameResult {
         .add_resource_path(path::PathBuf::from("./resources"));
 
     let (context, event_loop) = context_builder.build()?;
-
-    // Create the game state
-    let game = Game { world };
 
     // Run the main event loop
     event::run(context, event_loop, game)
--- src/rules.rs
+++ src/rules.rs
@@ -1,12 +1,12 @@
 use rand::{distributions::Uniform, Rng};
 
-type Generation = Vec<Vec<bool>>;
+pub type Generation = Vec<Vec<bool>>;
 
 //type rule_kernel = Vec<Vec<bool>>;
 
 //const rule_kernel: [[], ];
-const is_alive: bool = true;
-const is_dead: bool = false;
+pub const CELL_IS_ALIVE: bool = true;
+pub const CELL_IS_DEAD: bool = false;
 
 pub struct Universe {
     alive_neighbours: Vec<Vec<u8>>,
@@ -54,34 +54,32 @@ impl Universe {
         return self.alive_neighbours[0].len();
     }
 
-    // is_need_to_be_killed(current_cell, number_of_alive_neighbouts) -> bool
+    // fn is_need_to_be_killed(current_cell, number_of_alive_neighbouts) -> bool
     // {
     //     return current_cell_is_alive && (number_of_cell_neighbours < 2 || number_of_cell_neighbours > 3));
     // }
 
-    // is_need_to_be_alived(current_cell, number_of_alive_neighbouts) -> bool
+    // fn is_need_to_be_alived(current_cell, number_of_alive_neighbouts) -> bool
     // {
     //     return current_cell_is_dead && number_of_cell_neighbours == 3;
     // }
 
-
-
     // pub update_generation(mut& previous_generation) {
-        // assert_eq!() - check width
-        // assert_eq!() - check height
+    //     assert_eq!() - check width
+    //     assert_eq!() - check height
 
-        // convolve_with_rule_kernel(previous_generation)
-        //
-        // for (each_cell)
-        // {
-        //     if (is_need_to_be_alived(current_cell, get_number_of_alive_neighbours(position)))
-        //     {
-        //         make_current_cell_alive
-        //     } 
-        //     else if (is_need_to_be_killed(current_cell, getnumber_of_alive_neighbours(position)))
-        //     {
-        //         make_current_cell_dead
-        //     }
-        // }
+    //     let neighbours = convolve_with_rule_kernel(previous_generation);
+        
+    //     for (each_cell)
+    //     {
+    //         if (is_need_to_be_alived(current_cell, neighbours[]))
+    //         {
+    //             make_current_cell_alive
+    //         } 
+    //         else if (is_need_to_be_killed(current_cell, getnumber_of_alive_neighbours(position)))
+    //         {
+    //             make_current_cell_dead
+    //         }
+    //     }
     // }
 }
