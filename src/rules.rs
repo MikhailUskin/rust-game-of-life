bd9335ebfe7753030f21cdb95e06657eb0e29270
MikhailUskin <wurty@mail.ru>
2022-01-24 01:22:24 +0300

    upd: life on torus

--- src/rules.rs
+++ src/rules.rs
@@ -1,130 +1,146 @@
-use nalgebra::{matrix, SMatrix};
+use nalgebra::{matrix, SMatrix, Matrix, Vector4, Vector5, Vector2, Dim, DimName, Storage, RealField, MatrixMN};
+use nalgebra::base::dimension::{DimSum, DimAdd, DimSub, DimDiff, Const, ToConst, ToTypenum, U1};
+use nalgebra::base::allocator::Allocator;
+use nalgebra::base::default_allocator::DefaultAllocator;
 use rand::distributions::{Uniform};
 use std::cmp;
 use crate::constants::*;
 
+pub type RuleKernel = SMatrix<u8, RULE_KERNEL_WIDTH, RULE_KERNEL_HEIGHT>;
 pub type UniverseState = SMatrix<u8, UNIVERSE_WIDTH, UNIVERSE_HEIGHT>;
-pub type RuleKernel = SMatrix<u8, 3, 3>;
+
+const RULE_KERNEL_WIDTH_HALF: usize = RULE_KERNEL_WIDTH / 2;
+const RULE_KERNEL_HEIGHT_HALF: usize = RULE_KERNEL_HEIGHT / 2;
+const UNIVERSE_WRAPPED_WIDTH: usize = UNIVERSE_WIDTH + (2 * RULE_KERNEL_WIDTH_HALF);
+const UNIVERSE_WRAPPED_HEIGHT: usize = UNIVERSE_HEIGHT + (2 * RULE_KERNEL_HEIGHT_HALF);
+
+pub type UniverseWrapped = SMatrix<u8, UNIVERSE_WRAPPED_WIDTH, UNIVERSE_WRAPPED_HEIGHT>;
 
 pub const CELL_IS_ALIVE: u8 = 1;
 pub const CELL_IS_DEAD: u8 = 0;
 
-fn is_overlapped(first_shape: (usize, usize), second_shape: (usize, usize)) -> bool {
-    return first_shape != (0, 0) && second_shape != (0, 0);
-}
-
-pub fn convolve_full_wrap<const R1: usize, const C1: usize, const R2: usize, const C2: usize>(matrix: &SMatrix<u8, R1, C1>, kernel: &SMatrix<u8, R2, C2>) -> SMatrix<u8, R1, C1> {
-    let matrix_shape = matrix.shape();
-    let kernel_shape = kernel.shape();
-
-    if kernel_shape > matrix_shape {
-        panic!("convolve_full_wrap expects `self.shape() > kernel.shape()`, received {:?} and {:?} respectively.", matrix_shape, kernel_shape);
-    }
-
-    let mut convolve_result = SMatrix::<u8, R1, C1>::zeros();
-
-    let matrix_height = matrix.shape().0;
-    let matrix_width = matrix.shape().1;
-
-    let matrix_last_row_index = matrix_height - 1;
-    let matrix_last_column_index = matrix_width - 1;
-
+pub fn convolve_torus<const R1: usize, const C1: usize, const R2: usize, const C2: usize>(torus_wrapped_plane: & mut SMatrix<u8, R1, C1>, kernel: &SMatrix<u8, R2, C2>) -> SMatrix<u8, R1, C1>
+{
     let kernel_height = kernel.shape().0;
     let kernel_width = kernel.shape().1;
 
     let kernel_height_half = kernel_height / 2;
     let kernel_width_half = kernel_width / 2;
 
-    for row_index in 0..matrix_height
+    let torus_wrapped_plane_shape = torus_wrapped_plane.shape();
+
+    let torus_plane_height = torus_wrapped_plane_shape.0 - (2 * kernel_height_half);
+    let torus_plane_width = torus_wrapped_plane_shape.1 - (2 * kernel_width_half);
+
+    let torus_plane_shape = (torus_plane_height, torus_plane_width);
+    let kernel_shape = kernel.shape();
+
+    if kernel_shape > torus_plane_shape {
+        panic!("'convolve_on_torus' expects `torus_plane_shape.shape() > kernel.shape()`, received {:?} and {:?} respectively.", 
+            torus_plane_shape, kernel_shape);
+    }
+
+    // Corner shape (suppose kernel is square)
+
+    let corner_slice_shape = (kernel_height_half, kernel_width_half);
+
+    // Upper left corner
+
+    let upper_left_slice_target_position = (0, 0);
+    let upper_left_slice_source_position = (upper_left_slice_target_position.0 + torus_plane_height, upper_left_slice_target_position.1 + torus_plane_width);
+
+    let upper_left_slice_source = torus_wrapped_plane.slice(upper_left_slice_source_position, corner_slice_shape).clone_owned();
+    let mut upper_left_slice_target = torus_wrapped_plane.slice_mut(upper_left_slice_target_position, corner_slice_shape);
+    upper_left_slice_target.copy_from(&upper_left_slice_source);
+
+    // Upper right corner
+
+    let upper_right_slice_target_position = (0, kernel_width_half + torus_plane_width);
+    let upper_right_slice_source_position = (upper_right_slice_target_position.0 + torus_plane_height, upper_right_slice_target_position.1 - torus_plane_width);
+
+    let upper_right_slice_source = torus_wrapped_plane.slice(upper_right_slice_source_position, corner_slice_shape).clone_owned();
+    let mut upper_right_slice_target = torus_wrapped_plane.slice_mut(upper_right_slice_target_position, corner_slice_shape);
+    upper_right_slice_target.copy_from(&upper_right_slice_source);
+
+    // Bottom right corner
+
+    let bottom_right_slice_target_position = (kernel_height_half + torus_plane_height, kernel_width_half + torus_plane_width);
+    let bottom_right_slice_source_position = (bottom_right_slice_target_position.0 - torus_plane_height, bottom_right_slice_target_position.1 - torus_plane_width);
+
+    let bottom_right_slice_source = torus_wrapped_plane.slice(bottom_right_slice_source_position, corner_slice_shape).clone_owned();
+    let mut bottom_right_slice_target = torus_wrapped_plane.slice_mut(bottom_right_slice_target_position, corner_slice_shape);
+    bottom_right_slice_target.copy_from(&bottom_right_slice_source);
+
+    // Bottom left corner
+
+    let bottom_left_slice_target_position = (kernel_height_half + torus_plane_height, 0);
+    let bottom_left_slice_source_position = (bottom_left_slice_target_position.0 - torus_plane_height, bottom_left_slice_target_position.1 + torus_plane_width);
+
+    let bottom_left_slice_source = torus_wrapped_plane.slice(bottom_left_slice_source_position, corner_slice_shape).clone_owned();
+    let mut bottom_left_slice_target = torus_wrapped_plane.slice_mut(bottom_left_slice_target_position, corner_slice_shape);
+    bottom_left_slice_target.copy_from(&bottom_left_slice_source);
+
+    // Horizontal side shape
+
+    let horizontal_side_shape = (torus_plane_height, kernel_width_half);
+
+    // Left side
+
+    let left_side_target_position = (kernel_height_half, 0);
+    let left_side_source_position = (left_side_target_position.0, left_side_target_position.1 + torus_plane_width);
+
+    let left_side_slice_source = torus_wrapped_plane.slice(left_side_source_position, horizontal_side_shape).clone_owned();
+    let mut left_side_slice_target = torus_wrapped_plane.slice_mut(left_side_target_position, horizontal_side_shape);
+    left_side_slice_target.copy_from(&left_side_slice_source);
+
+    // Right side
+
+    let right_side_target_position = (kernel_height_half, kernel_width_half + torus_plane_width);
+    let right_side_source_position = (right_side_target_position.0, right_side_target_position.1 - torus_plane_width);
+
+    let right_side_slice_source = torus_wrapped_plane.slice(right_side_source_position, horizontal_side_shape).clone_owned();
+    let mut right_side_slice_target = torus_wrapped_plane.slice_mut(right_side_target_position, horizontal_side_shape);
+    right_side_slice_target.copy_from(&right_side_slice_source);
+
+    // Vertical side shape
+
+    let vertical_side_shape = (kernel_height_half, torus_plane_width);
+
+    // Upper side
+
+    let upper_side_target_position = (0, kernel_width_half);
+    let upper_side_source_position = (upper_side_target_position.0 + torus_plane_height, upper_side_target_position.1);
+
+    let upper_side_slice_source = torus_wrapped_plane.slice(upper_side_source_position, vertical_side_shape).clone_owned();
+    let mut upper_side_slice_target = torus_wrapped_plane.slice_mut(upper_side_target_position, vertical_side_shape);
+    upper_side_slice_target.copy_from(&upper_side_slice_source);
+
+    // Bottom side
+
+    let bottom_side_target_position = (kernel_height_half + torus_plane_height, kernel_width_half);
+    let bottom_side_source_position = (bottom_side_target_position.0 - torus_plane_height, bottom_side_target_position.1);
+
+    let bottom_side_slice_source = torus_wrapped_plane.slice(bottom_side_source_position, vertical_side_shape).clone_owned();
+    let mut bottom_side_slice_target = torus_wrapped_plane.slice_mut(bottom_side_target_position, vertical_side_shape);
+    bottom_side_slice_target.copy_from(&bottom_side_slice_source);
+
+    // Convolve
+
+    let mut convolve_result = SMatrix::<u8, R1, C1>::zeros();
+    let min_row_index = kernel_height_half;
+    let max_row_index = torus_plane_height + kernel_height_half;
+    let min_column_index = kernel_width_half;
+    let max_column_index = torus_plane_width + kernel_width_half;
+
+    for row_index in min_row_index..max_row_index
     {
-        for column_index in 0..matrix_width
+        for column_index in min_column_index..max_column_index
         {
-            let convolve_position = (row_index, column_index);
+            let convolve_slice_position = (row_index - kernel_height_half, column_index - kernel_width_half);
+            let matrix_slice = torus_wrapped_plane.slice(convolve_slice_position, kernel.shape());
 
-            // Local slice convolution
-
-            let local_slice_row = if convolve_position.0 > kernel_height_half { convolve_position.0 - kernel_height_half } else { 0 };
-            let local_slice_column = if convolve_position.1 > kernel_height_half { convolve_position.1 - kernel_height_half } else { 0 };
-            let local_slice_position = (local_slice_row, local_slice_column);
-
-            let upper_left_offset = (convolve_position.0 - local_slice_position.0, convolve_position.1 - local_slice_position.1);
-
-            let max_bottow_right_position = (convolve_position.0 + kernel_height_half, convolve_position.1 + kernel_width_half);
-            let bottom_right_offset = (cmp::min(max_bottow_right_position.0, matrix_last_row_index) - convolve_position.0, 
-                cmp::min(max_bottow_right_position.1, matrix_last_column_index) - convolve_position.1);
-
-            let local_shape = (bottom_right_offset.0 + upper_left_offset.0 + 1, bottom_right_offset.1 + upper_left_offset.1 + 1);
-            let local_matrix = matrix.slice(local_slice_position, local_shape);
-
-            let kernel_center = (kernel_height_half, kernel_width_half);
-            let local_kernel_position = (kernel_center.0 - upper_left_offset.0, kernel_center.1 - upper_left_offset.1);
-            let local_kernel = kernel.slice(local_kernel_position, local_shape);
-
-            let local_neighbours = local_matrix.dot(&local_kernel);
-
-            // Wrapped rows convolution
-
-            let mut wrapped_rows_matrix_position = (0, 0);
-            let mut wrapped_rows_kernel_position = (0, 0);
-            let mut wrapped_rows_shape = (0, 0);
-
-            if local_shape.0 < kernel_height {
-                let mut wrapped_rows_offset = 0;
-
-                if upper_left_offset.0 < kernel_height_half {
-                    wrapped_rows_offset = kernel_height_half - upper_left_offset.0;
-                    wrapped_rows_matrix_position = (matrix_height - wrapped_rows_offset, local_slice_position.1);
-                    wrapped_rows_kernel_position = (kernel_height - wrapped_rows_offset, local_kernel_position.1);
-                } else if bottom_right_offset.0 < kernel_height_half {
-                    wrapped_rows_offset = kernel_height_half - bottom_right_offset.0;
-                    wrapped_rows_matrix_position = (0 + wrapped_rows_offset - 1, local_slice_position.1);
-                    wrapped_rows_kernel_position = (0 + wrapped_rows_offset - 1, local_kernel_position.1);
-                }
-
-                wrapped_rows_shape = (wrapped_rows_offset, local_shape.1);
-            }
-
-            // Wrapped columns convolution
-
-            let mut wrapped_columns_matrix_position = (0, 0);
-            let mut wrapped_columns_kernel_position = (0, 0);
-            let mut wrapped_columns_shape = (0, 0);
-
-            if local_shape.1 < kernel_width {
-                let mut wrapped_columns_offset = 0;
-
-                if upper_left_offset.1 < kernel_width_half {
-                    wrapped_columns_offset = kernel_width_half - upper_left_offset.1;
-                    wrapped_columns_matrix_position = (local_slice_position.0, matrix_width - wrapped_columns_offset,);
-                    wrapped_columns_kernel_position = (local_kernel_position.0, kernel_width - wrapped_columns_offset);
-                } else if bottom_right_offset.1 < kernel_width_half {
-                    wrapped_columns_offset = kernel_width_half - bottom_right_offset.1;
-                    wrapped_columns_matrix_position = (local_slice_position.0, 0 + wrapped_columns_offset - 1);
-                    wrapped_columns_kernel_position = (local_kernel_position.0, 0 + wrapped_columns_offset - 1);
-                }
-
-                wrapped_columns_shape = (local_shape.0, wrapped_columns_offset);
-            }
-
-            let mut wrapped_rows_neighbours = 0u8;
-            let mut wrapped_columns_neighbours = 0u8;
-            let mut wrapped_overlapped_neighbours = 0u8;
-
-            if is_overlapped(wrapped_rows_shape, wrapped_columns_shape) {
-                // Do nothing
-            }
-            else if wrapped_rows_shape != (0, 0) {
-                let wrapped_rows_slice = matrix.slice(wrapped_rows_matrix_position, wrapped_rows_shape);
-                let wrapped_rows_kernel = kernel.slice(wrapped_rows_kernel_position, wrapped_rows_shape);
-                wrapped_rows_neighbours = wrapped_rows_slice.dot(&wrapped_rows_kernel);
-            } else if wrapped_columns_shape != (0, 0) {
-                let wrapped_rows_slice = matrix.slice(wrapped_columns_matrix_position, wrapped_columns_shape);
-                let wrapped_rows_kernel = kernel.slice(wrapped_columns_kernel_position, wrapped_columns_shape);
-                wrapped_columns_neighbours = wrapped_rows_slice.dot(&wrapped_rows_kernel);
-            }
-
-            convolve_result[(row_index, column_index)] = local_neighbours + 
-                wrapped_rows_neighbours + wrapped_columns_neighbours + wrapped_overlapped_neighbours;
+            let convolve_target_position = (row_index, column_index);
+            convolve_result[convolve_target_position] = matrix_slice.dot(&kernel);
         }
     }
 
@@ -136,43 +152,45 @@ const RULE_KERNEL: RuleKernel = matrix![1, 1, 1;
                                         1, 1, 1];
 
 pub struct Universe {
-    next_generation: UniverseState,
-    alive_neighbours: UniverseState,
+    next_generation_wrapped: UniverseWrapped,
+    alive_neighbours_wrapped: UniverseWrapped,
 }
 
 impl Universe {
-    fn seed_initial_generation() -> UniverseState {
+    fn seed_initial_generation() -> UniverseWrapped {
         let mut random_generator = rand::thread_rng();
         let uniform_range = Uniform::new_inclusive(0, 1);
-        let initial_generation: UniverseState = UniverseState::from_distribution(&uniform_range, &mut random_generator);
-
+        let initial_generation: UniverseWrapped = UniverseWrapped::from_distribution(&uniform_range, &mut random_generator);
+ 
         initial_generation
     }
 
     pub fn new() -> Self {
-        let initial_generation = Universe::seed_initial_generation(); 
-        let initial_neighbours = convolve_full_wrap(&initial_generation, &RULE_KERNEL);
+        let mut initial_generation = Universe::seed_initial_generation(); 
+        let initial_neighbours = convolve_torus(&mut initial_generation, &RULE_KERNEL);
 
         Universe {
-            next_generation: initial_generation,
-            alive_neighbours: initial_neighbours,
+            next_generation_wrapped: initial_generation,
+            alive_neighbours_wrapped: initial_neighbours,
         }
     }
 
     pub fn shape(&self) -> (usize, usize) {
-        self.next_generation.shape()
+        (UNIVERSE_HEIGHT, UNIVERSE_WIDTH)
     }
 
     pub fn revive_cell(&mut self, cell_row: u8, cell_column: u8) {
-        self.next_generation[(cell_row as usize, cell_column as usize)] = CELL_IS_ALIVE;
+        self.next_generation_wrapped[(cell_row as usize, cell_column as usize)] = CELL_IS_ALIVE;
     }
 
     pub fn kill_cell(&mut self, cell_row: u8, cell_column: u8) {
-        self.next_generation[(cell_row as usize, cell_column as usize)] = CELL_IS_DEAD;
+        self.next_generation_wrapped[(cell_row as usize, cell_column as usize)] = CELL_IS_DEAD;
     }
 
     pub fn get_cell_state(&self, cell_row: u8, cell_column: u8) -> u8 {
-        return self.next_generation[(cell_row as usize, cell_column as usize)];
+        let cell_row = (cell_row as usize) + RULE_KERNEL_HEIGHT_HALF;
+        let cell_column = (cell_column as usize) + RULE_KERNEL_WIDTH_HALF;
+        return self.next_generation_wrapped[(cell_row, cell_column)];
     }
 
     fn is_need_to_be_killed(current_cell: u8, number_of_alive_neighbours: u8) -> bool
@@ -186,8 +204,9 @@ impl Universe {
     }
 
     pub fn next_generation(&mut self) {
-        self.alive_neighbours = convolve_full_wrap(&self.next_generation, &RULE_KERNEL);
-        self.next_generation.zip_apply(&self.alive_neighbours, |current_cell_state, number_of_alive_neighbours| {
+        self.alive_neighbours_wrapped = convolve_torus(&mut self.next_generation_wrapped, &RULE_KERNEL);
+
+        self.next_generation_wrapped.zip_apply(&self.alive_neighbours_wrapped, |current_cell_state, number_of_alive_neighbours| {
             if Universe::is_need_to_be_alived(*current_cell_state, number_of_alive_neighbours)
             {
                 *current_cell_state = CELL_IS_ALIVE;
